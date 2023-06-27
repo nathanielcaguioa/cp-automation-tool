a9a2f1529db4bac8906fdc0121a41dc3848090d5
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-06-27 22:53:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ import sys
 import os
 import pandas as pd
 
+def envVarCheck(envVar):
+  if os.environ[envVar] is null:
+    envVar = "NA"
+  else:
+    envVar = os.environ[envVar]
+  return envVar
+
 inputToolAction = os.environ['Execution']
 inputServiceCheck = os.environ['Service Check']
 inputServerType = os.environ['Server Type']
@@ -14,9 +21,13 @@ print(inputServerType)
 #inputHotfix2012 = (os.environ.get(['HotfixID_2012']))
 #inputHotfix2019 = (os.environ.get(['HotfixID_2019']))
 
-server = os.environ.get('HotfixID_2012', 'youtube.com')
-server2 = os.environ.get('HotfixID_2019', 'NA')
+#server = os.environ.get('HotfixID_2012', 'youtube.com')
+#server2 = os.environ.get('HotfixID_2019', 'NA')
+
+Env2019 = envVarCheck('HotfixID_2019')
+print(Env2019)
 
 
-print(server)
-print(server2)
+
+
+
