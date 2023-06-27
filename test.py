141908176284d2f517014e73a148b9f6716d5264
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-06-27 20:16:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ import pandas as pd
 
 inputToolAction = os.environ['Execution']
 inputServiceCheck = os.environ['Service Check']
+inputServiceType = os.environ['Service Type']
+mandatory_env_var = ["HotfixId_2019", "HotfixId_2012", "Service Name"]
 print(inputToolAction)
 print(inputServiceCheck)
-if 'HotfixId_2019' in os.environ:
-    print('Environment variable exists!')
-else:
-    print('Environment variable does not exist.')
+
+print(os.environ.get('HotfixId_2019', default_value))
+print(os.environ.get('HotfixId_2012', default_value))
+
