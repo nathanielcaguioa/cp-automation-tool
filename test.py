171f20c7a5208f0436fb7927aea214010ae0d37b
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-06-27 22:21:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ import pandas as pd
 inputToolAction = os.environ['Execution']
 inputServiceCheck = os.environ['Service Check']
 #inputServiceType = os.environ['Server Type']
-mandatory_env_var = ["HotfixId_2019", "HotfixId_2012", "Service Name"]
 default_value="NA"
 print(inputToolAction)
 print(inputServiceCheck)
 
-print(os.environ.get('HotfixId_2019', default_value))
-print(os.environ.get('HotfixId_2012', default_value))
+inputHotfix2012 = (os.environ.get('HotfixId_2019', default_value))
+inputHotfix2019 = (os.environ.get('HotfixId_2012', default_value))
 
+print(inputHotfix2012)
+print(inputHotfix2019)
