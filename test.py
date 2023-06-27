29ea628355b48f626ec523c826657a56f406599c
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-06-27 23:07:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -16,8 +16,8 @@ def envVarCheck(envVar):
 inputToolAction = os.environ['Execution']
 inputServiceCheck = os.environ['Service Check']
 inputServerType = os.environ['Server Type']
-inputHotfix2012 = os.environ['HotfixID_2012']
-inputHotfix2019 = os.environ['HotfixID_2019']
+inputHotfix2012 = os.environ['HotfixId2012']
+inputHotfix2019 = os.environ['HotfixId2019']
 print(inputToolAction)
 print(inputHotfix2012)
 print(inputHotfix2019)
@@ -27,10 +27,10 @@ print(inputHotfix2019)
 #server = os.environ.get('HotfixID_2012', 'youtube.com')
 #server2 = os.environ.get('HotfixID_2019', 'NA')
 
-Env2019 = envVarCheck('HotfixID_2019')
-Env2012 = envVarCheck('HotfixID_2012')
-print(Env2019)
-print(Env2012)
+Env_new = envVarCheck('HotfixId2019')
+Env_old = envVarCheck('HotfixId2012')
+print(Env_old)
+print(Env_new)
 
 
 
