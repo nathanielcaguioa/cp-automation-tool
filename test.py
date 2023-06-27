93d9661dac7fff1cced4face5c50a7f6fb2e5a7e
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-06-27 19:02:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,11 @@ import sys
 import os
 import pandas as pd
 
-from os import environ
 inputToolAction = os.environ['Execution']
 inputServiceCheck = os.environ['Service Check']
 print(inputToolAction)
 print(inputServiceCheck)
-REQUIRED_ENV_VARS = {"HotfixId_2019", "HotfixId_2012"}
-diff = REQUIRED_ENV_VARS.difference(environ)
-if len(diff) > 0:
-    raise EnvironmentError(f'Failed because {diff} are not set')
+if 'HotfixId_2019' in os.environ:
+    print('Environment variable exists!')
+else:
+    print('Environment variable does not exist.')
