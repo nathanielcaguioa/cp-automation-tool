8fe544afb1b8d6ccbdf57e4e4e982b54a7fdef5d
Dennis-Cresido-Deltek <136627343+Dennis-Cresido-Deltek@users.noreply.github.com>
2023-07-03 17:37:44 +0800

    Add files via upload

---
+++ cp-prepatching-tool.py
@@ -0,0 +1,95 @@
+import boto3
+import csv
+import sys
+import os
+import pandas as pd
+
+def mainFunction():
+
+    csvFile = 'cp-automation-tool-server-list.csv'
+
+    main_USEA_list, main_USWE_list = sortServerList(csvFile)
+
+    if not main_USEA_list:
+        print ("No USEA server in the list.")
+    else:
+        setSSMCommandSetting(main_USEA_list,'us-east-1')
+    
+    if not main_USWE_list:
+        print ("No USWE server in the list.")
+    else:
+        setSSMCommandSetting(main_USWE_list,'us-west-2')
+
+def setSSMCommandSetting(set_serverlist,setRegion):
+
+    from datetime import date
+    setDocument = "cp-installwindowsupdate"
+    ssm_client = boto3.client('ssm',region_name=setRegion)
+    runToday = date.today()
+    runToday = runToday.strftime("%d%m%Y")
+    setComment = ("Prepatching Function" + "-" +runToday)
+
+    runSSMCommand(ssm_client,set_serverlist,setComment,setDocument)
+
+
+def verInstance(verInstanceId,verRegion):
+    
+    try:
+        ec2_client = boto3.client('ec2',region_name=verRegion)
+        response = ec2_client.describe_instance_status(
+            InstanceIds=[verInstanceId],
+        )
+        return "OKAY"
+    except:
+        return ("An exception occurred")
+
+
+def caseRegion(region):
+    if region == 'USEA':
+        return "us-east-1"
+    elif region == 'USWE':
+        return "us-west-2"
+    elif region == 'ALL':
+        return "all"     
+
+def sortServerList(sortCSVfile):
+
+    filter_df = pd.read_csv(sortCSVfile)
+
+
+    sortUSEAInstances = []
+    sortUSWEInstances = []
+
+
+    for index, row in filter_df.iterrows():
+        rowServerName = row['servername']
+        rowInstanceId = row['instanceid']
+        rowRegion = rowServerName[0:4]
+
+        sortRegion = caseRegion(rowRegion)
+
+        sortServerExist = verInstance(rowInstanceId,sortRegion)
+        if sortServerExist == "OKAY" and rowRegion == "USEA":
+            sortUSEAInstances.append(rowInstanceId)
+        elif sortServerExist == "OKAY" and rowRegion == "USWE":
+            sortUSWEInstances.append(rowInstanceId)
+    
+    return sortUSEAInstances,sortUSWEInstances
+
+def runSSMCommand(runSession,runInstances,runComment,runDocument):
+    from datetime import date
+    
+
+    
+    response = runSession.send_command(
+                    Targets=[{"Key": "InstanceIds", "Values": runInstances}],
+                    Comment=runComment,
+                    DocumentName=runDocument,
+                    MaxConcurrency='100%',
+                    MaxErrors='100%',
+                    TimeoutSeconds=900)
+
+    command_id = response['Command']['CommandId']
+    print("Command Id:" + command_id)
+
+mainFunction()
